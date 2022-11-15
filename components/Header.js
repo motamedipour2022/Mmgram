ea40ad7f0278e9c2da2f2104ab23e7019da5a133
Mohsen Motamedipour <m.motamedipour2022@yahoo.com>
2022-11-15 09:33:22 +0330

    make base header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 import React from 'react'
+import { SearchIcon } from '@heroicons/react/outline'
 
 export default function Header() {
   return <div>
     {/* left */}
     <div className='flex items-center justify-between max-w-6xl'>
-        <div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid'>
+        <div className='cursor-pointer h-28 w-28 relative hidden lg:inline-grid'>
             <Image
                 src="https://toppng.com/uploads/preview/instagram-text-115510536100dsjrkf7zx.png"
                 layout='fill'
@@ -20,6 +21,12 @@ export default function Header() {
                 className='object-contain'
             />
 
+        </div>
+        <div className='relative mt-1' >
+                <div className='absolute top-2 left-4 '>
+                    <SearchIcon className='h-5 text-gray-500 '/>
+                </div>
+                <input type="text" placeholder='Search' className='bg-gray-50 pl-11 text-center border-gray-500 rounded-md text-sm focus:ring-black focus:border-black ' />
         </div>
         <h1>Right side</h1>
     </div>
