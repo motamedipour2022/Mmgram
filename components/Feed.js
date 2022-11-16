6f54bf865df307e0cf03baf3a340fe64436c9278
Mohsen Motamedipour <m.motamedipour2022@yahoo.com>
2022-11-16 10:12:36 +0330

    finish the Header and story

---
+++ components/Feed.js
@@ -0,0 +1,15 @@
+import Stories from "../components/Stories"
+
+export default function Feed() {
+  return (
+    <main>
+        <section>
+            <Stories/>
+        </section>
+        <section>
+
+        </section>
+
+    </main>
+  )
+}
